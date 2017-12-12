01e94f44e736eb0427a5ba4b71a3e60732cfa6d0
d.rabellino <thefuncraft@gmail.com>
2017-12-12 14:38:55 +0100

    Modif reglage

--- bdd.php
+++ bdd.php
@@ -20,15 +20,7 @@ class Mysql{
     $link = '',
     $debogue = true,
     $nb_query = 0;
-    /**
-    * Constructeur de la classe
-    * Connexion aux serveur de base de donnée et sélection de la base
-    *
-    * $Serveur     = L'hôte (ordinateur sur lequel Mysql est installé)
-    * $Bdd         = Le nom de la base de données
-    * $Identifiant = Le nom d'utilisateur
-    * $Mdp         = Le mot de passe
-    */
+
     public function __construct($server = 'localhost', $data_base = 'base', $id = 'root', $password = ''){
         $this->server = $server;
         $this->data_base = $data_base;
@@ -43,17 +35,6 @@ class Mysql{
             throw new MySQLExeption(' Erreur de connexion à la base de donnees!!! ');
     }
 
-    /**
-    * Retourne le nombre de requêtes SQL effectué par l'objet
-    */
-    public function RetourneNbRequetes(){
-        return $this->nb_query;
-    }
-    /**
-    * Envoie une requête SQL et récupère le résultât dans un tableau pré formaté
-    *
-    * $Requete = Requête SQL
-    */
     public function TabResSQL($query){
         $i = 0;
         $ressource = mysqli_query($this->link, $query);
@@ -67,18 +48,7 @@ class Mysql{
         $this->nb_query++;
         return $tabResult;
     }
-    /**
-    * Retourne le dernier identifiant généré par un champ de type AUTO_INCREMENT
-    *
-    */
-    public function DernierId(){
-        return mysqli_insert_id($this->link);
-    }
-    /**
-    * Envoie une requête SQL et retourne le nombre de table affecté
-    *
-    * $Requete = Requête SQL
-    */
+
     public function ExecuteSQL($query){
         $ressource = mysqli_query($this->link,$query);
         if (!$ressource and $this->debogue) throw new MySQLExeption(' Erreur de requête SQL!!! ');
@@ -86,4 +56,12 @@ class Mysql{
         $nbAffectee = mysqli_affected_rows($this->link);
         return $nbAffectee;
     }  
+
+    public function RetourneNbRequetes(){
+        return $this->nb_query;
+    }
+
+    public function DernierId(){
+        return mysqli_insert_id($this->link);
+    }
 }
--- tools.php
+++ tools.php
@@ -17,13 +17,13 @@
     }
 
     function getAllIds($table){
-        $dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+        $dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
         $query = "select id from " . $table;
         return $dblink->TabResSQL($query);
     }
 
     function getAllNom($table){
-        $dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+        $dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
         $query = "select nom from " . $table;
         return $dblink->TabResSQL($query);
     }
@@ -48,7 +48,7 @@
 	// INDEX
 
 	function getIntroFilm(){
-        $dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+        $dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
         $query = "select f.id,titre,chemin from film f, film_has_photo fhp, photo p where f.id=fhp.id_film and fhp.id_photo=p.id and fhp.role='affiche' order by titre";
         return $res = $dblink->TabResSQL($query);
     }
@@ -56,7 +56,7 @@
     // FILM.PHP
 
 	function getFilm($id){
-		$dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+		$dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
 		$query = "select * from film where id=$id";
 		$res = $dblink->TabResSQL($query);
 		$res[0]['dateSortie'] = formatDate($res[0]['dateSortie']);
@@ -65,19 +65,19 @@
 	}
 
     function getPhotoFilm($id){
-        $dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+        $dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
         $query = "select chemin, legende, role from photo p, film_has_photo f where p.id=f.id_photo and f.id_film=$id order by role";
         return $dblink->TabResSQL($query);
     }
 
     function getParticipant($role,$idFilm){
-        $dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+        $dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
         $query = "select prenom, nom, chemin, legende from personne p, film_has_personne fhp, personne_has_photo php, photo ph where p.id=fhp.id_personne and p.id=php.id_personne and php.id_photo=ph.id and role ='$role' and id_film=$idFilm order by prenom";
         return $dblink->TabResSQL($query);
     }
 
-	function getPersonne($nom){
-		$dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+	function getPerson($nom){
+		$dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
 		$query = "select prenom, nom, dateNaissance, biographie, chemin from personne pr, personne_has_photo php, photo p where pr.id=php.id_personne and php.id_photo=p.id and nom='$nom'";
 		$res = $dblink->TabResSQL($query);
 		for ($i = 0; $i < count($res); ++$i) {
@@ -87,7 +87,7 @@
 	}
 
 	function getFilmographie($nom){
-        $dblink = new Mysql('db708477891.db.1and1.com','db708477891','dbo708477891','Fairytail21!');
+        $dblink = new Mysql('mysql-deams.alwaysdata.net','deams_sitephp','deams','toto');
         $query = "select f.id, chemin, titre from photo p, film_has_photo fhp, film f, film_has_personne fhr, personne pr where pr.id=fhr.id_personne and fhr.id_film=f.id and f.id=fhp.id_film and fhp.id_photo=p.id and pr.nom='$nom' and fhp.role='affiche' order by titre";
         return $dblink->TabResSQL($query);
     }
